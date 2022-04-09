411b6a81d3ae92a926e7984f6acd438ac626d6f4
Yash Agrawal <52177580+yash88600@users.noreply.github.com>
2022-04-10 00:19:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+// function password_check()
+// {
+//     console.log("hello");
+//     if (document.getElementById('password').value == document.getElementById('confirm_password').value)
+//     {
+        
+//         document.getElementById('confirm_password').style.border = '1px solid green';
+//     }
+//     else
+//     {
+//         document.getElementById('confirm_password').style.border = '1px solid red';
+//     }
+// }
+
+
+// password_check();
+
+console.log("dasdasss");
